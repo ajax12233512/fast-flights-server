af0dad8b777bb911a9aadcacb36a1cc69f5f5743
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-11 19:26:30 -0500

    update gitignore

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
 app.listen(port, () => {
     console.log('Server running');
 })
