b0640de3a691b960395b40d86003a8bfe7498166
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-04-13 13:55:20 -0500

    server implemetation for future api routes

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { application } from 'express';
 const app = express();
 const port = process.env.PORT || 3001;
 import { duffel } from './duffel/duffel.js'
 
 const testDuffel = async () => {
   try{
-    const airlines = await duffel.airlines.list();
-    console.log(airlines)
+    const airports = await duffel.airports.listWithGenerator();
+    for await (const airport of airports) 
+      console.log(airport)  
   } catch (err) {
     console.log(err)
   }
 
 }
 
+app.use(express.json());
+
+app.post('/api/duffel', async (req, res) => {
+  try{
+    const airports = await duffel.airports.listWithGenerator(10);
+    for await (const airport of airports) {
+      if(airport.data.city_name.includes(req.body.input))
+        res.json(airport);
+    }
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 app.get('/api/test', async (req, res) => {
   res.json({
